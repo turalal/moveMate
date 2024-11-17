52398e852ae6be132ab8885f0df391d6d6423127
Ismat <ismetsemedov@gmail.com>
2024-11-17 15:34:51 +0400

    check

--- movemate/authentication/admin.py
+++ movemate/authentication/admin.py
@@ -1,3 +1,32 @@
+# authentication/admin.py
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'phone', 'is_active', 'is_staff', 'created_at')
+    list_filter = ('is_active', 'is_staff', 'is_superuser', 'created_at', 'groups')
+    search_fields = ('username', 'first_name', 'last_name', 'email', 'phone')
+    ordering = ('-created_at',)
+    
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        (_('Personal info'), {'fields': ('first_name', 'last_name', 'email', 'phone')}),
+        (_('Permissions'), {
+            'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions'),
+        }),
+        (_('Important dates'), {'fields': ('last_login', 'date_joined', 'created_at', 'updated_at')}),
+    )
+    
+    readonly_fields = ('created_at', 'updated_at', 'last_login', 'date_joined')
+    
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'phone', 'password1', 'password2'),
+        }),
+    )
+
+    filter_horizontal = ('groups', 'user_permissions',)
--- movemate/pages/admin.py
+++ movemate/pages/admin.py
@@ -1,3 +1,38 @@
+# pages/admin.py
 from django.contrib import admin
+from .models import Contact, Service, BlogPost, BlogCategory, Comment
 
-# Register your models here.
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ('name', 'email', 'created_at')
+    list_filter = ('created_at',)
+    search_fields = ('name', 'email', 'message')
+    readonly_fields = ('created_at',)
+
+@admin.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'is_active', 'created_at')
+    list_filter = ('is_active', 'created_at')
+    search_fields = ('title', 'description')
+    prepopulated_fields = {'slug': ('title',)}
+
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_at')
+    search_fields = ('name',)
+    prepopulated_fields = {'slug': ('name',)}
+
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'category', 'status', 'created_at')
+    list_filter = ('status', 'category', 'created_at')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('author', 'post', 'created_at', 'is_approved')
+    list_filter = ('is_approved', 'created_at')
+    search_fields = ('content', 'author__username', 'post__title')
+    raw_id_fields = ('author', 'post')
